f536fa644995c4a7ed021bf1d884dc7d26c64ca4
one0955 <63694834+one0955@users.noreply.github.com>
2020-06-14 17:44:33 +0900

    Update Oblock.java

--- Oblock.java
+++ Oblock.java
@@ -1,9 +1,8 @@
 
 public class Oblock{
-	private short position;
-	private short[][] board;
+	private byte position;
 	int type;
-	private short pos;
+	private byte pos;
 	Oblock(int type){						// 시작시 정보
 		this.type = type;
 		if(type==3) {		//type은 3 과 4가 될것임
@@ -11,27 +10,29 @@ public class Oblock{
 		}
 		else position = 33;
 	}
+	Oblock(int type, byte pos){
+		this.type = type;
+		position =pos;
+	}
 	
-	
-	boolean checkAvailable() {
+	boolean checkAvailable(Board board) {
 		if(pos == position) return true;	//같은 O블럭 즉, 이동하지 않을때
-		if(board[pos/10][pos%10]!=0) return false;	// 빈 블록으로만 이동할 것임.
+		if(board.board[pos/10][pos%10]!=0) return false;	// 빈 블록으로만 이동할 것임.
 		return true;
 	}
 	
 	
-	boolean update() {				// 블럭을 놓을 수 있는 지를 확인 한 후에 업데이트함. 업데이트를 했으면 true 못했으면 false를 리턴함
-		if(checkAvailable()) {
+	boolean update(Board board) {				// 블럭을 놓을 수 있는 지를 확인 한 후에 업데이트함. 업데이트를 했으면 true 못했으면 false를 리턴함
+		if(checkAvailable(board)) {
 			position=pos;
 			return true;
 		}
 		return false;
 	}
-	short getPosition() {
+	byte getPosition() {
 		return position;
 	}
-	void setTempInfo(short[][] board, short pos) {
-		this.board = board;
+	void setTempInfo(byte pos) {
 		this.pos = pos;
 	}
 }

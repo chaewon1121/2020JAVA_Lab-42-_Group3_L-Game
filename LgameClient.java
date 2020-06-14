daee8f435b3354aa68973149c0cf4fc7e13bf69f
ht0324 <49386658+ht0324@users.noreply.github.com>
2020-06-14 21:09:24 +0900

    Update LgameClient.java

--- LgameClient.java
+++ LgameClient.java
@@ -118,6 +118,9 @@ public class LgameClient {
 
 	        // The textarea will be inside this panel
 	        JPanel areaPanel = new JPanel(new BorderLayout());
+		
+		// bottompanel for status and info button
+		JPanel bottomPanel = new JPanel(new BorderLayout());
 
 	        JLabel playerinfo = new JLabel("<html>Player id: "+yourId+"<br/>Player color: " + ((yourId==1) ? "Red" : "Blue")+"<html>" );
 	        playerinfo.setBorder(new LineBorder(Color.BLACK));
@@ -135,6 +138,12 @@ public class LgameClient {
 	        JButton nextButton = new JButton("Next");
 	        nextButton.setActionCommand("Next");
 	        nextButton.addActionListener(new ButtonClickListener(statusBar));
+		    
+		//adding info button
+	        JButton infoButton = new JButton("Info");
+	    	infoButton.setActionCommand("Info");
+	    	infoButton.addActionListener(new ButtonClickListener(statusBar));
+	        bottomPanel.add(infoButton, BorderLayout.WEST);
 	        
 	        // Fill the whole space of the panel with the area
 	        areaPanel.add(nextButton, BorderLayout.EAST);
@@ -161,7 +170,9 @@ public class LgameClient {
 	        // The panel with the buttons should fill the remaining space
 	        mainPanel.add(buttonPanel, BorderLayout.CENTER);
 	        
-	        mainPanel.add(statusBar, BorderLayout.SOUTH);
+		bottomPanel.add(statusBar, BorderLayout.CENTER);
+	        
+	        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
 
 	        getContentPane().add(mainPanel);
 	        
@@ -224,6 +235,14 @@ public class LgameClient {
 	    			posPointer=0;//reset pospointer
 	    			myTurn=false;
 	    			 changeStatus("next!");
+	    		}
+			else if (command.equals("Info")) {
+	    			try {
+						new InfoUi();//open infoui window
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 	    		}
 	    		}
 	    	}
